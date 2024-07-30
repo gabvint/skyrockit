b48ff4ad8c43c17fd511c9ba6d147a5485b3d0e1
gabtongol <geybtongol@gmail.com>
2024-07-30 17:13:43 -0400

    full app without css

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 const authController = require('./controllers/auth.js');
+const applicationsController = require('./controllers/applications.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -16,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
@@ -28,21 +32,25 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
+app.use(passUserToView)
 
-app.get('/vip-lounge', (req, res) => {
+// server.js
+app.get('/', (req, res) => {
+  // Check if the user is signed in
   if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
+    // Redirect signed-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
   } else {
-    res.send('Sorry, no guests allowed.');
+    // Show the homepage for users who are not signed in
+    res.render('index.ejs');
   }
 });
 
+
 app.use('/auth', authController);
+app.use(isSignedIn)
+app.use('/users/:userId/applications', applicationsController)
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
